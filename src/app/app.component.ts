6835a7b937a3a33f1e6050f121d99e1a8db2b86f
Johann Kazal <johannkazal@gmail.com>
2020-06-08 14:26:52 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import * as jexcel from "jexcel";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TESTPROJ';
+  @ViewChild("spreadsheet") spreadsheet: ElementRef;
+  title = "CodeSandbox";
+ 
+  ngAfterViewInit() {
+    jexcel(this.spreadsheet.nativeElement, {
+      data: [[]],
+      minDimensions: [10, 10],
+      onchange : this.change
+    });
+  }
+
+  change (instance, cell, x, y, value) { var cellName = jexcel.getColumnNameFromId([x,y]); console.log('New change on cell ' + cellName + ' to: ' + value); }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { fr_FR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+
+registerLocaleData(fr);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
